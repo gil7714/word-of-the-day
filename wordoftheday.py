15a722a269c7b4a994ac5a0db709d671cf8b37cd
gil7714 <128074352+gil7714@users.noreply.github.com>
2023-03-25 13:35:00 -0400

    Add files via upload

---
+++ wordoftheday.py
@@ -0,0 +1,18 @@
+import json
+import re
+
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.merriam-webster.com/word-of-the-day'
+respone = requests.get(url, timeout=5)
+content = BeautifulSoup(respone.content, "html.parser")
+
+
+for word in content.find_all('h2', attrs={"class": "word-header-txt"}):
+    print(word.text)
+
+
+definition = content.find("div", attrs={"class": "wod-definition-container"})
+deftext = definition.find_next("p")
+print(deftext.text)
